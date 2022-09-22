ff0335400e5dd78cc8af229d23efe09f207208cc
--replace-all <--replace-all>
2022-09-22 10:54:18 +0600

    additional fixes

--- js/script.js
+++ js/script.js
@@ -15,7 +15,12 @@ const onGenerateSubmit = (e) => {
         showSpinner();
         setTimeout(() => {
             hideSpinner();
-            generateQrCode(url, size); 
+            generateQrCode(url, size);
+
+            setTimeout(() => {
+                const saveUrl = qr.querySelector('img').src;
+                createSaveBtn(saveUrl);
+            }, 50);
         }, 1000);
     }
 };
@@ -38,13 +43,19 @@ const hideSpinner = () => {
 
 const clearUI = () => {
     qr.innerHTML = '';
+    const saveLink = document.getElementById('save-link');
+    if(saveLink) saveLink.remove();
 }
 
 const createSaveBtn = (saveUrl) => {
     const link = document.createElement('a');
     link.id = 'save-link';
-    link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3'
-}
+    link.classList = 'bg-green-500 hover:bg-green-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+    link.href = saveUrl;
+    link.download = 'qrcode';
+    link.innerHTML = 'Save Image';
+    document.getElementById('generated').appendChild(link);
+};
 
 hideSpinner();
 
